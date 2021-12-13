efe99f705f47165368c219fe3e75973ad8f178bb
Callista Christ <callistachrist@Callistas-iMac.local>
2021-12-13 10:51:44 -0600

    achieving decent accuracy

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,22 @@
 import pandas as pd
 
 def load_data(path):
+    '''
+    Loads csv into Pandas DataFrame
+    '''
     return pd.read_csv(path)
+
+def format_axes(plt):
+    '''
+    Formats plot axes
+    '''
+    plt.rcParams['axes.linewidth'] = 3
+    plt.rcParams['xtick.major.width'] = 2
+    plt.rcParams['ytick.major.width'] = 2
+    plt.rcParams['xtick.minor.width'] = 2
+    plt.rcParams['ytick.minor.width'] = 2
+    plt.rc('xtick.major', size=8, pad=8)
+    plt.rc('xtick.minor', size=6, pad=5)
+    plt.rc('ytick.major', size=8, pad=8)
+    plt.rc('ytick.minor', size=6, pad=5)
+    return plt
